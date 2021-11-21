bcc0e0340882f0282c2de6003de4198b11e0407b
Rizqiyanto <backenddev@solutechglobal.com>
2021-11-21 22:52:20 +0700

    init commit

--- src/server.js
+++ src/server.js
@@ -5,35 +5,33 @@ const utils = require('./utils');
 
 // fn to create express server
 const create = async () => {
+  // server
+  const app = express();
+  app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 
-    // server
-    const app = express();
-    app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
-    
-    // Log request
-    app.use(utils.appLogger);
+  // Log request
+  app.use(utils.appLogger);
 
-    // root route - serve static file
-    app.get('/api/hello', (req, res) => {
-        res.json({hello: 'goodbye'});
-        res.end();
-    });
+  // root route - serve static file
+  app.get('/api/hello', (req, res) => {
+    res.json({ hello: 'goodbye' });
+    res.end();
+  });
 
-    // root route - serve static file
-    app.get('/', (req, res) => {
-        return res.sendFile(path.join(__dirname, '../public/client.html'));
+  // root route - serve static file
+  app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, '../public/client.html'));
+  });
 
-    });
+  // Catch errors
+  app.use(utils.logErrors);
+  app.use(utils.clientError404Handler);
+  app.use(utils.clientError500Handler);
+  app.use(utils.errorHandler);
 
-    // Catch errors
-    app.use(utils.logErrors);
-    app.use(utils.clientError404Handler);
-    app.use(utils.clientError500Handler);
-    app.use(utils.errorHandler);
-
-    return app;
+  return app;
 };
 
 module.exports = {
-    create
+  create,
 };
